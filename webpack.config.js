dea832ae85b23e7e6b034a2038a8a5c951435569
Dmytro Malyovaniy <plastiniq@gmail.com>
2021-11-22 05:40:55 +0200

    refactor: Install Bootstrap

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                  'vue-style-loader',
+                  'css-loader',
+                  {
+                    loader: 'sass-loader',
+                  }
+                ]
+            }
+        ]
+    },
+}
